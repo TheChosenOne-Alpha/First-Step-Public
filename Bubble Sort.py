9e46dd659902368cc6e4a11ecb3df5f56891b25b
TheChosenOne-Alpha <jacobkw0907@gmail.com>
2024-11-05 22:49:41 -0500

    Bubble Sort 2.0.py

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -1,27 +1,17 @@
 def bubble_sort(list):
-
+    sorted = 0
     #loop until all list value checked
-    sorted = False
-
-    while sorted == False:
-        sorted = True
-        for i in range(len(list) - 1):
+    for i in range(len(list) - 1):
+        for i in range(len(list) - 1 - sorted):
+            print(i)
             if list[i] > list[i + 1]:
-                sorted = False
 
-        if sorted == False:
-            for i in range(len(list) - 1):
-                print(i)
-                if list[i] > list[i + 1]:
-
-                    #swap
-                    list[i], list[i + 1] = list[i + 1], list[i]
-                    print(list)
-    
-    print("Final:", list)
-    return list
+                #swap
+                list[i], list[i + 1] = list[i + 1], list[i]
+                print(list)
+        sorted += 1
 
 
 
-a = [5,1,2,6,0,3] #change this list to see other number sorting well
+a = [5,1,2,6,0,3,8,3] #change this list to see other number sorting well
 bubble_sort(a)
